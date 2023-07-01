9341fa7200fa9370276ec592a346f719c162055b
CGMW293 <138163744+CGMW293@users.noreply.github.com>
2023-07-01 15:44:07 -0700

    Main.cpp

--- Fuckoff.cpp
+++ Fuckoff.cpp
@@ -1,27 +1,38 @@
 #include <windows.h>
+#include <cstdlib> 
+#include <ctime> 
 
 int main()
 {
-	POINT pos;//Structure for the position of a cursor.
-	int width = 1080;//Gets X value of resolution. Helps with scaling jitter for different resolutions
-	int moveval = (width / 540) * 1.5; //idk i made this shit up |3 for 1080|11 for 4k |;
-	while (1) {
-		int direction = rand() % 4 + 1;
-		GetCursorPos(&pos);
-		switch (direction) {
-		case(4):
-			SetCursorPos(pos.x - moveval, pos.y - moveval);
-			break;
-		case(1):
-			SetCursorPos(pos.x + moveval, pos.y + moveval);
-			break;
-		case(2):
-			SetCursorPos(pos.x + moveval, pos.y - moveval);
-			break;
-		case(3):
-			SetCursorPos(pos.x - moveval, pos.y + moveval);
-			break;
-		}
-		Sleep(pos.x / (direction * 20));
-	}
+
+    srand(time(NULL)); // seed for random number generator :)
+    POINT pos;//structure for mouse position
+    while (1) {
+        GetCursorPos(&pos);
+
+        //these four help for Multi-Monitor Displays
+        HMONITOR wild = MonitorFromPoint(pos, MONITOR_DEFAULTTONEAREST);
+        MONITORINFO Moninfo = {sizeof(Moninfo)};
+        GetMonitorInfo(wild, &Moninfo);
+        int screenWidth = Moninfo.rcMonitor.right - Moninfo.rcMonitor.left; //mm hmmm seems about right
+
+        int moveval = (screenWidth / 1080);//Helps scale with different sized displays
+        int direction = rand() % 4 + 1;
+        switch (direction) {
+        case(4):
+            SetCursorPos(pos.x - moveval, pos.y - moveval);
+            break;
+        case(1):
+            SetCursorPos(pos.x + moveval, pos.y + moveval);
+            break;
+        case(2):
+            SetCursorPos(pos.x + moveval, pos.y - moveval);
+            break;
+        case(3):
+            SetCursorPos(pos.x - moveval, pos.y + moveval);
+            break;
+        }
+        int sleepTime = rand() % 251; // sleep time between 0 and 250 milliseconds, gives the "Burst" feeling
+        Sleep(sleepTime);
+    }
 }
